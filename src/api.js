3d371e73ece31c4ce47c01a766ed8ce4ced20729
Kumudhini Reddicherla <kumudhini.reddicherla@gmail.com>
2025-02-10 23:39:14 -0500

    Updated api file

--- src/api.js
+++ src/api.js
@@ -1,7 +1,9 @@
 // src/api.js
 
 // fragments microservice API to use, defaults to localhost:8080 if not set in env
-const apiUrl = process.env.API_URL || `http://ec2-44-211-170-29.compute-1.amazonaws.com:8080`;
+const apiUrl =
+  process.env.API_URL ||
+  `${window.location.protocol}//${window.location.hostname}:8080`;
 
 /**
  * Given an authenticated user, request all fragments for this user from the
@@ -11,16 +13,32 @@ const apiUrl = process.env.API_URL || `http://ec2-44-211-170-29.compute-1.amazon
 export async function getUserFragments(user) {
   console.log('Requesting user fragments data...');
   try {
+
+    const token = user.idToken || sessionStorage.getItem("id_token");
     const res = await fetch(`${apiUrl}/v1/fragments`, {
       // Generate headers with the proper Authorization bearer token to pass.
       // We are using the `authorizationHeaders()` helper method we defined
       // earlier, to automatically attach the user's ID token.
-      headers: user.authorizationHeaders(),
+      headers: {
+        Authorization: `Bearer ${token}`, // Use the `id_token` as the Bearer token
+        "Content-Type": "application/json",
+      },
+      method: "GET",
     });
     if (!res.ok) {
       throw new Error(`${res.status} ${res.statusText}`);
     }
+
     const data = await res.json();
+
+    if (data.fragments.length > 0) {
+      console.log(
+        `Successfully retrieved ${data.fragments.length} fragment(s):`,
+        { data }
+      );
+    } else {
+      console.log("No fragments found for the user.");
+    }
     console.log('Successfully got user fragments data', { data });
     return data;
   } catch (err) {
